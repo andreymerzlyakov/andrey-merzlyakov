dcbd0d41df4cd6b99b6daefe057ed70b9f515ffb
andreymerzlyakov <andreymerzlykov2007@gmail.com>
2025-06-16 14:49:22 +0400

    1

--- let-const.js
+++ let-const.js
@@ -10,7 +10,11 @@
 
 // Код задания 1
 
-
+const appName = "Name";
+let currentUser = "login";
+currentUser = "logout";
+let isDarkTheme = true;
+isDarkTheme = false;
 
 /**
  * Задание 2.
@@ -23,7 +27,14 @@
 
 // Код задания 2
 
+const userName = "Ivan";
+let age = "18";
+let status = "login";
+// userName = "Roma";
+// изменение переменной вызавает ошибку, потому что const создает константу, доступную только для чтения
+let hello = "Привет, " + userName + "!";
 
+console.log(hello)
 
 /**
  * Задание 3.
@@ -34,6 +45,11 @@
 
 // Код задания 3
 
+const userCount = 10;
+// userCount += 20; Ошибка
+// const создает ссылку на значение доступную только для чтения
+let mostUserCount = userCount + 10; console.log(mostUserCount); // способ исправления
+console.log(userCount);
 
 /** Задание 4.
  *  Что выведет следующий код?
@@ -51,3 +67,6 @@
 
 // Ответ на задание 4
 
+10
+20
+30
